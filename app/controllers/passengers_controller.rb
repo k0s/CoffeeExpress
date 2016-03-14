d5560d0f302e8bd98dec8a29ff5d293a9a03b0ad
Jeff Hammel <k0scist@gmail.com>
2016-03-14 14:23:00 -0700

    more WIP

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,7 +1,11 @@
 class PassengersController < ApplicationController
+
+  # new passenger form
   def new
   end
 
+  # add a new passenger
   def create
+    render plain: params[:passenger].inspect
   end
 end
